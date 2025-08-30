5c645889e128bbec95ea1ec39ee9827c115b2aef
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-08-30 13:22:38 -0300

    Introdução às Bases de Dados no Laravel e Criando e conectando a uma base de dados MySQL e SQLite

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,14 @@
 <?php
 
-use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    try {
+        DB::connection()->getPdo();
+
+        echo "conectado com sucesso!";
+    } catch (Exception $e) {
+        echo "Erro ao conectar: " . $e->getMessage();
+    }
+});
